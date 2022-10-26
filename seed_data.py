4dadba877028501e37806962d02afa1f5d06ffc7
BethzabethM <bethbetancourt@hotmail.com>
2022-10-26 16:50:24 -0300

    comiteando 1

--- seed_data.py
+++ seed_data.py
@@ -1,8 +1,8 @@
 from ejemplo.models import Familiar
 
-Familiar(nombre="Anibal", direccion="Gabrieles 98-4b", numero_pasaporte=12172548).save()
-Familiar(nombre="Carmen", direccion="Gabrieles 98-4b", numero_pasaporte=3660032).save()
-Familiar(nombre="Barbara", direccion="Gabrieles 98-4b", numero_pasaporte=10785673).save()
-Familiar(nombre="Clara", direccion="Gabrieles 98-4b", numero_pasaporte=3289657).save()
+Familiar(nombre="Anibal", direccion="Gabrieles 98-4b", numero_pasaporte=12172548, fecha_nacimiento="26/08/1974").save()
+Familiar(nombre="Carmen", direccion="Gabrieles 98-4b", numero_pasaporte=3660032, fecha_nacimiento="08/09/1947").save()
+Familiar(nombre="Barbara", direccion="Gabrieles 98-4b", numero_pasaporte=10785673, fecha_nacimiento="31/06/1975").save()
+Familiar(nombre="Clara", direccion="Gabrieles 98-4b", numero_pasaporte=3289657, fecha_nacimiento="31/03/1943").save()
 
 print("Se cargo con exito los usuarios de pruebas")
